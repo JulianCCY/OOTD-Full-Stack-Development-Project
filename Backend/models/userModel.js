70f5aee54e74e56c7cfa9b577284a380a2899dc3
Boris Hoi <Boris.Hoi@metropolia.fi>
2021-12-17 02:32:46 +0200

    changed post username position

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -28,7 +28,6 @@ var bcrypt = require('bcryptjs');
   const getAllUsers = async () => {
     //all async will return a promise
     try {
-      // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
       const [rows] = await promisePool.query('SELECT * FROM ootd_user');
       return rows;
     } catch (e) {
